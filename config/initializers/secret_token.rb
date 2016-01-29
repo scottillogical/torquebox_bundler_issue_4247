0df7652424e8090ea1554fe25f1053dbba186d3e
Scott Schulthess <sschulthess@Zipmac044.local>
2016-01-29 11:05:51 -0500

    initial import, works in toruebox 2.3.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TorqueboxExamples::Application.config.secret_token = 'd9c74e8d74f187565253b093e4507369e7a667bf89d5c7bbfdc8d65402de53b5a6e8b10aa0c866cf45cb23521d9e9e31309ceb3a9144b1c401d0377f00c074d7'
